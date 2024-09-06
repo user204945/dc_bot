e932090cdfb67a68990799b6993de975d12c855e
user204945 <rellionz@proton.me>
2024-09-06 14:01:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -299,57 +299,6 @@ client.on('interactionCreate', async (interaction) => {
     } else {
         await interaction.reply({ content: 'Geçersiz etkileşim!', ephemeral: true });
     }
-
-    if (interaction.type === InteractionType.ApplicationCommand) {
-        const command = client.commands.get(interaction.commandName);
-        if (command) {
-            try {
-                await command.execute(interaction);
-            } catch (error) {
-                console.error(error);
-                await interaction.reply({ content: 'Bir hata oluştu!', ephemeral: true });
-            }
-        }
-    }
-
-    if (interaction.isButton()) {
-        const { customId } = interaction;
-
-        if (customId === 'eğlence') {
-            const funEmbed = new EmbedBuilder()
-                .setColor('#00FF00')
-                .setTitle('Eğlence Komutları')
-                .setDescription('Aşağıda eğlence ile ilgili komutları bulabilirsiniz.')
-                .addFields(
-                    { name: '.patla', value: 'Sahte patlama komutu.', inline: true },
-                    { name: '.ip <kullanıcı>', value: 'Sahte IP adresi üretir.', inline: true },
-                    { name: '.kaç-cm <kullanıcı>', value: 'Rastgele yarrak boyu üretir.', inline: true },
-                    { name: '.sik <kullanıcı>', value: 'Belirtilen kişiyi siker.', inline: true }
-                )
-                .setTimestamp();
-
-            await interaction.update({ embeds: [funEmbed], components: [] });
-        } else if (customId === 'yetkili') {
-            const modEmbed = new EmbedBuilder()
-                .setColor('#FF0000')
-                .setTitle('Yetkili Komutları')
-                .setDescription('Aşağıda yetkili ile ilgili komutları bulabilirsiniz.')
-                .addFields(
-                    { name: '.ban <kullanıcı> <sebep>', value: 'Kullanıcıyı yasaklar.', inline: true },
-                    { name: '.kick <kullanıcı> <sebep>', value: 'Kullanıcıyı sunucudan atar.', inline: true },
-                    { name: '.jail <kullanıcı> <süre> <sebep>', value: 'Kullanıcıya jail atar.', inline: true },
-                    { name: '.unjail <kullanıcı>', value: 'Kullanıcının jailini kaldırır.', inline: true },
-                    { name: '.k <kullanıcı id> <isim> <yaş>', value: 'Kullanıcıyı kayıt eder.', inline: true },
-                    { name: '.k-sil <kullanıcı id>', value: 'Kullanıcının kaydını siler.', inline: true },
-                    { name: '.nuke', value: 'Mevcut kanalı siler ve aynısını tekrardan açar.', inline: true },
-                    { name: '.sil <miktar>', value: 'Belirtilen miktarda mesajı siler.', inline: true },
-                    { name: '.lock', value: 'Belirtilen kanalı kilitler.', inline: true }
-                )
-                .setTimestamp();
-
-            await interaction.update({ embeds: [modEmbed], components: [] });
-        }
-    }
 });
 
 
