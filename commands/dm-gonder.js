f1af56568115d731ed5b9da1d32db523f1b623f6
user204945 <rellionz@proton.me>
2024-09-07 03:46:39 +0300

    Update dm-gonder.js

--- commands/dm-gonder.js
+++ commands/dm-gonder.js
@@ -6,17 +6,18 @@ module.exports = {
     description: 'Belirtilen kullanıcıya DM gönderir.',
     async execute(message, args) {
         
+        // Bot sahibi olup olmadığını kontrol et
         if (message.author.id !== config.botOwnerId) {
             return message.reply('Bu komutu kullanma izniniz yok.');
         }
 
-        
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])?.user;
+        // ID ve mesaj al
+        const userId = args[0];
         const dmMessage = args.slice(1).join(' ');
 
-        
-        if (!user) {
-            return message.reply('Lütfen geçerli bir kullanıcı belirtin.');
+        // Kullanıcı ID'si ve mesajın kontrolü
+        if (!userId) {
+            return message.reply('Lütfen geçerli bir kullanıcı ID\'si belirtin.');
         }
 
         if (!dmMessage) {
@@ -24,10 +25,13 @@ module.exports = {
         }
 
         try {
-            
+            // Kullanıcıyı ID ile al
+            const user = await message.client.users.fetch(userId);
+
+            // Mesaj gönder
             await user.send(dmMessage);
 
-            
+            // Başarıyla gönderildiğini bildiren embed
             const embed = new EmbedBuilder()
                 .setColor('#00FF00')
                 .setTitle('DM Gönderildi')
@@ -38,7 +42,7 @@ module.exports = {
 
         } catch (error) {
             console.error('DM gönderme sırasında bir hata oluştu:', error);
-            message.reply('DM gönderilirken bir hata oluştu. Kullanıcının DM almayı kapatmış olabilir.');
+            message.reply('DM gönderilirken bir hata oluştu. Kullanıcının DM almayı kapatmış olabilir veya kullanıcı bulunamadı.');
         }
     },
 };
